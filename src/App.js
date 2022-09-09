e469bde9eabcd8ad0729d8baf33bcbf31366041a
zaki88-ahmed <zaki.ahmed88@gmail.com>
2022-09-09 20:56:55 +0200

    after_routig

--- src/App.js
+++ src/App.js
@@ -9,20 +9,37 @@ import Cart from './Component/Cart/Cart';
 import Contact from './Component/Contact/Contact';
 import Services from './Component/Services/Services';
 import Blog from './Component/Blog/Blog';
+import {BrowserRouter, Route, Routes} from "react-router-dom";  
 
 class App extends Component{
   render(){
 
-      return <div>
-        <Navbar/>
-        {/* <Index/> */}
-        {/* <Shop/> */}
-        {/* <Aboutus/> */}
-        {/* <Cart/> */}
-        {/* <Contact/> */}
-        {/* <Services/> */}
-        <Blog/>
-      </div>
+    return <BrowserRouter>
+      <Navbar/>
+      <Routes>
+
+        <Route path="/" element={ <Index/> } />
+        <Route path="/shop" element={ <Shop/> } />
+        <Route path="/aboutus" element={ <Aboutus/> } />
+        <Route path="/cart" element={ <Cart/> } />
+        <Route path="/contact" element={ <Contact/> } />
+        <Route path="/services" element={ <Services/> } />
+        <Route path="/blog" element={ <Blog/> } />
+
+      </Routes>
+      </BrowserRouter>
+
+
+      // return <div>
+      //   <Navbar/>
+      //   {/* <Index/> */}
+      //   {/* <Shop/> */}
+      //   {/* <Aboutus/> */}
+      //   {/* <Cart/> */}
+      //   {/* <Contact/> */}
+      //   {/* <Services/> */}
+      //   <Blog/>
+      // </div>
 
   }
 }
--- src/Component/Navbar.js
+++ src/Component/Navbar.js
@@ -1,6 +1,7 @@
 import React, { Component }  from 'react';
 import userImage from '../Images/user.svg';
 import cart from '../Images/cart.svg';
+import {Link} from "react-router-dom";
 
 class Navbar extends Component{
   render(){
@@ -17,18 +18,30 @@ class Navbar extends Component{
 		<div class="collapse navbar-collapse" id="navbarsFurni">
 			<ul class="custom-navbar-nav navbar-nav ms-auto mb-2 mb-md-0">
 				<li class="nav-item active">
-					<a class="nav-link" href="index.html">Home</a>
+					{/* <a class="nav-link" href="index.html">Home</a> */}
+					<Link class="navbar-brand" to="/">Home</Link>
 				</li>
-				<li><a class="nav-link" href="shop.html">Shop</a></li>
-				<li><a class="nav-link" href="about.html">About us</a></li>
-				<li><a class="nav-link" href="services.html">Services</a></li>
-				<li><a class="nav-link" href="blog.html">Blog</a></li>
-				<li><a class="nav-link" href="contact.html">Contact us</a></li>
+				<li>
+					<Link class="nav-link" to="/shop">Shop</Link>
+				</li>
+				<li>
+					<Link class="nav-link" to="/aboutus">About Us</Link>
+				</li>
+				<li>
+					<Link class="nav-link" to="/services">Services</Link>
+				</li>
+				<li>
+					<Link class="nav-link" to="/blog">Blog</Link>
+				</li>
+
+				
 			</ul>
 
 			<ul class="custom-navbar-cta navbar-nav mb-2 mb-md-0 ms-5">
 				<li><a class="nav-link" href="#"><img src={userImage}/></a></li>
-				<li><a class="nav-link" href="cart.html"><img src={cart}/></a></li>
+				<li>
+					<Link class="nav-link" to="/cart"><img src={cart}/></Link>
+				</li>
 			</ul>
 		</div>
 	</div>
